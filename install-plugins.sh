7328cc3e514748e55c52f07e38f60d04f338958c
Nikhil Lanjewar <nikhil@yournextleap.com>
2012-10-08 16:27:42 +0530

    Add command files and install instructions

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 NAGIOS_EXECUTABLES=/usr/local/nagios/libexec
+NAGIOS_COMMANDS=/usr/local/nagios/etc/commands
+
 cp -Rv plugins/* $NAGIOS_EXECUTABLES/
 chown -R nagios.nagios $NAGIOS_EXECUTABLES
+
+cp -Rv commands/* $NAGIOS_COMMANDS
